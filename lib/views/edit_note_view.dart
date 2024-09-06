456b406dcda02bf948eab8237044164f323ed8e9
taha <asdfghmnt@gmail.com>
2024-09-06 22:00:44 +0300

    edite note view ui done

--- lib/views/edit_note_view.dart
+++ lib/views/edit_note_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note/widgets/custom_app_bar.dart';
+import 'package:note/widgets/custom_text_field.dart';
 
 class EditNoteViwe extends StatelessWidget {
   const EditNoteViwe({super.key});
@@ -8,6 +9,18 @@ class EditNoteViwe extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: customAppBar(Icons.check, 'Edit Note'),
+      body: const Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 20),
+            child: CustomTextField(hintText: 'Title'),
+          ),
+          CustomTextField(
+            hintText: 'Content',
+            maxLine: 5,
+          )
+        ],
+      ),
     );
   }
 }
